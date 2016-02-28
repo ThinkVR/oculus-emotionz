ecfac23f7abb3ba594575a18256d8ce05799276e
Saif Ali Khan <saifullah.a.khan@gmail.com>
2016-02-27 16:21:49 -0800

    Unity App
    Track and visualize your emotions based on brainwaves

---
+++ Assets/raycastScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class raycastScript : MonoBehaviour {
+	RaycastHit hit;
+	private Vector3 crosshairPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		if (Physics.Raycast(transform.position, fwd, out hit, 100)) {
+			if (hit.collider.name == "MagicLamp") {
+				Debug.Log ("You ordered food!");
+			}
+		}
+
+		Debug.DrawRay (transform.position, fwd, Color.blue);
+
+	}
+}
